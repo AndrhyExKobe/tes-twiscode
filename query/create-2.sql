54c1a0d1410ff8952592ecd513442a4dfcddc33d
andri dwi utomo <andri_dwiutomo@ymail.com>
2022-05-05 14:34:36 +0800

    project twiscode

--- query/create-2.sql
+++ query/create-2.sql
@@ -1,3 +1,3 @@
-create table detail_transaction(id int auto increment primary key, id_transaksi int FOREIGN KEY REFERENCES transaksi(id), harga int, jumlah int, subtotal int);
+create table detail_transaction(id int auto_increment primary key, id_transaksi int, harga int, jumlah int, subtotal int, Foreign Key(id_transaksi) references transaksi(id))
 
 INSERT INTO detail_transaction(id_transaksi,harga,jumlah,subtotal) VALUES (1, 1000, 10, 100), (1, 1000, 10, 100), (1, 1000, 10, 100);
