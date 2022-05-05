806385715d1525af76355317cb1e73ffa35136ab
andri dwi utomo <andri_dwiutomo@ymail.com>
2022-05-05 14:39:31 +0800

    project twiscode

--- query/create-1.sql
+++ query/create-1.sql
@@ -1,3 +1,3 @@
-create table transaksi(id int auto increment primary key, tanggal_order date, status_pelunasan boolean default false, tanggal_pembayaran date);
+create table transaksi(id int auto increment primary key, tanggal_order datetime, status_pelunasan boolean default false, tanggal_pembayaran datetime);
 
-INSERT INTO transaksi(tanggal_order,status_pelunasan,tanggal_pembayaran) VALUES ('2022-01-01',true,'2022-02-02'),('2022-01-01',false,'2022-02-02'),('2022-01-01',false,'2022-02-02');
+INSERT INTO transaksi(tanggal_order,status_pelunasan,tanggal_pembayaran) VALUES ('2022-01-01 02:00:00',true,'2022-02-02 01:00:00'),('2022-01-01 03:00:00',false,'2022-02-02 01:00:00'),('2022-01-01 01:00:00',false,'2022-02-02 01:00:00');
